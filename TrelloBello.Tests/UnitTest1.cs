f82c1502dfd36f65e9fa08e16bb4580455451856
Peter Michaleczky <peter.michaleczky@pactera.com>
2017-02-18 11:13:08 +0100

    At payload request all commit's is written to Trello as comments

--- TrelloBello.Tests/UnitTest1.cs
+++ TrelloBello.Tests/UnitTest1.cs
@@ -2,22 +2,37 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Configuration;
 using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using TrelloBello.Models.Github;
 
 namespace TrelloBello.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void Integ_TrelloService_AddComment()
-        {            
-            var apiKey = ConfigurationManager.AppSettings.Get("TRELLO_API_KEY");
-            var token = ConfigurationManager.AppSettings.Get("TRELLO_OAUTH_TOKEN");
-            var testCardId = ConfigurationManager.AppSettings.Get("TrelloTestCardId");
-            var trello = new Services.TrelloService(apiKey, token);
-            var result = trello.AddCommentToCard(testCardId, "$Új kommit:123455667891132(testbranch)\n$unittest run)");
-            var json = JObject.Parse(result);
-        }
+        //[TestMethod]
+        //public void Integ_TrelloService_AddComment()
+        //{            
+        //    var apiKey = ConfigurationManager.AppSettings.Get("TRELLO_API_KEY");
+        //    var token = ConfigurationManager.AppSettings.Get("TRELLO_OAUTH_TOKEN");
+        //    var testCardId = ConfigurationManager.AppSettings.Get("TrelloTestCardId");
+        //    var trello = new Services.TrelloService(apiKey, token);
+        //    var result = trello.AddCommentToCard(testCardId, "$Új kommit:123455667891132(testbranch)\n$unittest run)");
+        //    var json = JObject.Parse(result);
+        //}
+
+        //[TestMethod]        
+        //[DeploymentItem("github_payload.json")]
+        //public void Integ_Github_Webhook()
+        //{
+        //    var payloadJson = System.IO.File.ReadAllText("github_payload.json");
+        //    var payload = JsonConvert.DeserializeObject<Models.Github.Payload>(payloadJson);
+        //    var controller = new Controllers.GithubController();
+        //    var task = controller.PostPayload(payload);
+        //    task.Wait();
+        //    var result = task.Result;
+        //}
 
     }
 }
--- TrelloBello/Controllers/GithubController.cs
+++ TrelloBello/Controllers/GithubController.cs
@@ -4,10 +4,12 @@ using MongoDB.Driver;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web.Http;
 using TrelloBello.Data;
@@ -25,15 +27,37 @@ namespace TrelloBello.Controllers
         }
 
         public async Task<IHttpActionResult> PostPayload(Models.Github.Payload payload)
-        {            
+        {
             await _db.GithubPayloads.InsertOneAsync(payload);
-            return Ok(new { Status = "OK", Payload = payload});
+            if (payload.Commits != null)
+            {
+                foreach (var commit in payload.Commits)
+                {
+                    var cardId = _ExtractTrelloLink(commit.Message);
+                    if (!String.IsNullOrEmpty(cardId))
+                    {
+                        var trello = new Services.TrelloService(ConfigurationManager.AppSettings.Get("TRELLO_API_KEY"), ConfigurationManager.AppSettings.Get("TRELLO_OAUTH_TOKEN"));
+                        var commentText = String.Format("$Új kommit: {0}", commit.Id);
+                        trello.AddCommentToCard(cardId, commentText);
+                    }
+                }
+            }
+            return Ok(new { Status = "OK", Github = payload });
         }
 
         public IHttpActionResult GetPayload()
         {
             var commits = _db.GithubPayloads.AsQueryable().SelectMany(x => x.Commits).ToList();
             return Ok(commits);
+        }
+
+        private string _ExtractTrelloLink(string commitMessage)
+        {
+            var urlExtractRegExp = @"(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])";
+            var trelloUrlPattern = @"trello.com\/c\/(........)";
+            var trelloUrl = Regex.Match(commitMessage, urlExtractRegExp, RegexOptions.IgnoreCase);
+            var matches = Regex.Split(trelloUrl.Value, trelloUrlPattern, RegexOptions.IgnoreCase);
+            return matches[1];
         }
 
     }
